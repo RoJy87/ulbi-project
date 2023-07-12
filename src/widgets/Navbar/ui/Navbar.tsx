b0814e0503bd5a175e72a50ac0d04224c39d1355
Danil Simonov <simdanila@gmailcom>
2023-07-12 23:53:05 +0300

    add react testing library, test for btn, sidebar

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -1,5 +1,6 @@
 import { classNames } from 'shared/lib/classNames/classNames';
 import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
+import { t } from 'i18next';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
@@ -10,10 +11,10 @@ export const Navbar = ({ className }: NavbarProps) => (
     <div className={classNames(cls.Navbar, {}, [className])}>
         <div className={cls.links}>
             <AppLink theme={AppLinkTheme.SECONDARY} to="/" className={cls.mainLink}>
-                Главная
+                {t('Главная')}
             </AppLink>
             <AppLink theme={AppLinkTheme.RED} to="/about">
-                О сайте
+                {t('О сайте')}
             </AppLink>
         </div>
     </div>
